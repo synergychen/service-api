aefd2d3fbc0d876b027f64d33ea0c287feb228d8
Chen Huang <alan.tolearn@gmail.com>
2016-05-11 21:03:20 -0400

    Move users and posts under namespace api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :posts, except: [:new, :edit]
-  resources :users, except: [:new, :edit]
+  namespace :api do
+    resources :posts, except: [:new, :edit]
+    resources :users, except: [:new, :edit]
+  end
 end
